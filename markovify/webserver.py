cc5ad907233fe3a0b3649b56bfc4aa317f2e2f3f
Suhail Ansari <suhail.ism@gmail.com>
2017-04-09 21:22:41 -0700

    added markovify template

--- markovify/webserver.py
+++ markovify/webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import pickle
 
 
@@ -10,13 +10,14 @@ with open('es-2.model', 'rb') as f:
 with open('en-2.model', 'rb') as f:
   models['en'] = pickle.load(f)
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 
 @app.route('/')
 @app.route('/<lang>')
 @app.route('/<lang>/<int:tries>')
 def index(lang='es', tries=10):
   print('markov lang=%s tries=%d' % (lang, tries))
-  return models[lang].make_sentence(tries=tries) or ''
+  sentence = models[lang].make_sentence(tries=tries) or ''
+  return render_template('index.html', sentence=sentence)
 
 app.run(port=5001, host='0.0.0.0')
